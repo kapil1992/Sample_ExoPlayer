cc8792abc22c35d7e68442b3c545fcbde523890a
Kapil Jain <kapil.jain@iwedia.com>
2022-01-27 13:55:30 +0530

    Sample_ExoPlayer for OTT playback.
    Signed-off-by: Kapil Jain <kapil.jain@iwedia.com>

---
+++ app/src/main/java/com/example/exoplayerotttest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.exoplayerotttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    // creating a variable for exoplayerview.
+    SimpleExoPlayerView exoPlayerView;
+
+    // creating a variable for exoplayer
+    SimpleExoPlayer exoPlayer;
+
+    String videoURL = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+    //String videoURL = "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        exoPlayerView = findViewById(R.id.idExoPlayerVIew);
+        try {
+
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "ExoPlayer"));
+
+            // we are parsing a video url and parsing its video uri.
+            Uri videouri = Uri.parse(videoURL);
+
+            DashMediaSource dashMediaSource = new DashMediaSource(videouri, dataSourceFactory,
+                    new DefaultDashChunkSource.Factory(dataSourceFactory), null, null);
+
+            // bandwisthmeter is used for getting default bandwidth
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+            // track selector is used to navigate between video using a default seekbar.
+            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+
+            // we are adding our track selector to exoplayer.
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+            /*// we are creating a variable for datasource factory and setting its user agent as 'exoplayer_view'
+            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+
+            // we are creating a variable for extractor factory and setting it to default extractor factory.
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+            // we are creating a media source with above variables and passing our event handler as null,
+            MediaSource mediaSource = new ExtractorMediaSource(videouri, dataSourceFactory, extractorsFactory, null, null);*/
+
+            // inside our exoplayer view we are setting our player
+            exoPlayerView.setPlayer(exoPlayer);
+
+            // we are preparing our exoplayer with media source.
+            exoPlayer.prepare(dashMediaSource);
+
+            // we are setting our exoplayer when it is ready.
+            exoPlayer.setPlayWhenReady(true);
+
+        } catch (Exception e) {
+            // below line is used for handling our errors.
+            Log.e("ExoPlayer", "Error : " + e.toString());
+        }
+    }
+}
